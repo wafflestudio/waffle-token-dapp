01aec51520991e9d3e30312520046aa4b5d554b2
GoGiants1 <chooi221@snu.ac.kr>
2022-01-23 00:17:22 +0900

    Resolve web3 errors - https://github.com/ChainSafe/web3.js#web3-and-create-react-app

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,12 +1,27 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
+
 import { Helmet } from 'react-helmet-async';
 
 export function HomePage() {
+  const [account, setAccount] = useState<string>(''); // state variable to set account.
+
+  useEffect(() => {
+    async function load() {
+      const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
+      const accounts = await web3.eth.requestAccounts();
+
+      setAccount(accounts[0]);
+    }
+
+    load();
+  }, []);
+
   return (
     <>
       <Helmet>
-        <title>HomePage</title>
-        <meta name="description" content="A Boilerplate application homepage" />
+        <title>MainPage</title>
+        <meta name="description" content="WaffleToken Main Page" />
       </Helmet>
       <span>My HomePage</span>
     </>
