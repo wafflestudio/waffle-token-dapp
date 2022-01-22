844025b145fb209df7d9d5ce5d292b3d6434e225
GoGiants1 <chooi221@snu.ac.kr>
2022-01-23 02:12:43 +0900

    Add header component

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -2,19 +2,39 @@ import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
 
 import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components/macro';
+import Header from 'app/components/Commons/Header';
 
 export function HomePage() {
-  const [account, setAccount] = useState<string>(''); // state variable to set account.
+  const Account = styled.div`
+    font-size: 1.5em;
+    text-align: center;
+    color: cornflowerblue;
+  `;
+
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
+
+  const [account, setAccount] = useState<string>('');
+
+  async function loadAccount() {
+    // const web3 = new Web3(Web3.givenProvider || 'ws://localhost:7545'); // local
+    const web3 = new Web3(
+      Web3.givenProvider ||
+        'ws://https://ropsten.infura.io/v3/e5a2aa837b5b4b02b4a2a4acaa09e6ca',
+    );
+    const accounts = await web3.eth.requestAccounts();
+    console.log(accounts);
+
+    setAccount(accounts[0]);
+  }
+
+  async function loadBalance() {}
 
   useEffect(() => {
-    async function load() {
-      const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
-      const accounts = await web3.eth.requestAccounts();
-
-      setAccount(accounts[0]);
-    }
-
-    load();
+    loadAccount();
   }, []);
 
   return (
@@ -23,7 +43,10 @@ export function HomePage() {
         <title>MainPage</title>
         <meta name="description" content="WaffleToken Main Page" />
       </Helmet>
-      <span>My HomePage</span>
+      <Container>
+        <Header />
+        <Account draggable="true">your account is {account}</Account>
+      </Container>
     </>
   );
 }
